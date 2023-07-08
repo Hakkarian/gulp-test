23c09fea85a11643a4ffca91fc10b26e59e6c386
Hakkar <zugrutus@gmail.com>
2023-07-08 23:22:56 +0100

    css-header-hero

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,32 +1,65 @@
 const gulp = require('gulp');
 const htmlmin = require("gulp-htmlmin");
 const imagemin = require("gulp-imagemin");
+const svgmin = require('gulp-svgmin');
+const svgstore = require('gulp-svgstore')
 const sass = require("gulp-sass")(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const ts = require('gulp-typescript');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const inject = require('gulp-inject');
+const server = require('browser-sync');
+const browserSync = require('browser-sync').create();
+
 
 gulp.task("html", () =>
   gulp
     .src("src/*.html")
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest("dist"))
+    .pipe(gulp.dest("dist")).pipe(browserSync.stream())
 );
 
 gulp.task("scss", () => gulp.src("src/scss/**/*.scss").pipe(sass().on('error', sass.logError)).pipe(gulp.dest("dist/css")));
-gulp.task("mincss", () => gulp.src("dist/css/**/*.css").pipe(cleanCSS()).pipe(gulp.dest('dist/css')));
+gulp.task("mincss", () =>
+  gulp
+    .src("dist/css/**/*.css")
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("dist/css"))
+    .pipe(browserSync.stream())
+);
 
 gulp.task("css", (done) => gulp.series("scss", "mincss")(done))
-gulp.task('babel', () => gulp.src("src/ts/**/*.ts").pipe(ts()).pipe(babel()).pipe(uglify()).pipe(gulp.dest("dist/js")));
+gulp.task("babel", () =>
+  gulp
+    .src("src/ts/**/*.ts")
+    .pipe(ts())
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(gulp.dest("dist/js"))
+    .pipe(browserSync.stream())
+);
 
 gulp.task("images", () =>
-  gulp.src("src/images/*").pipe(imagemin()).pipe(gulp.dest("dist/images"))
+  gulp
+    .src("src/images/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/images").pipe(browserSync.stream()))
 );
 
 gulp.task("desk-images", () =>
-  gulp.src("src/images/desktop/*").pipe(imagemin()).pipe(gulp.dest("dist/images/desktop"))
+  gulp
+    .src("src/images/desktop/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/images/desktop").pipe(browserSync.stream()))
+);
+
+gulp.task("svg", () =>
+  gulp
+    .src("src/svg/*svg")
+    .pipe(svgmin())
+    .pipe(svgstore())
+    .pipe(gulp.dest("dist/svg"))
 );
 
 gulp.task('watch', () => {
@@ -37,4 +70,20 @@ gulp.task('watch', () => {
   gulp.watch("src/images/desktop/*", gulp.series("desk-images"));
 })
 
-gulp.task('default', gulp.series("watch"));
+gulp.task("server", () => {
+  browserSync.init({
+    server: {
+      baseDir: "dist",
+      open: true,
+      notify: false,
+    },
+    port: 3003,
+    open: true,
+    notify: false,
+  })
+  gulp.watch("dist/*.html").on("change", browserSync.reload);
+  gulp.watch("dist/css/*.css").on("change", browserSync.reload);
+}
+);
+
+gulp.task("default", gulp.series("server", "watch"));
--- src/ts/modal.ts
+++ src/ts/modal.ts
@@ -1,7 +1,20 @@
-const button = document.querySelector('.button');
+const openBtn = document.querySelector(".menu-open");
+const closeBtn = document.querySelector(".menu-close");
+const mobMenu = document.querySelector(".mob-menu");
 
 const handleToggle = () => {
-    
-}
+    console.log('menu', mobMenu)
+  if (openBtn?.classList.contains("is-hidden-button")) {
+    openBtn?.classList.remove("is-hidden-button");
+      closeBtn?.classList.add("is-hidden-button");
+      mobMenu?.classList.add("is-hidden");
+  } else {
+      openBtn?.classList.add("is-hidden-button");
+      closeBtn?.classList.remove("is-hidden-button");
+      mobMenu?.classList.remove("is-hidden");
+  }
+};
 
-button?.addEventListener('click', handleToggle);
+openBtn?.addEventListener("click", handleToggle);
+closeBtn?.addEventListener("click", handleToggle);
+
