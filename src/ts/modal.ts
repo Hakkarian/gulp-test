200501683921c01fbc8605240f606ebb232856ff
Hakkar <zugrutus@gmail.com>
2023-07-10 23:45:30 +0100

    mob-fin-desk-start

--- src/ts/modal.ts
+++ src/ts/modal.ts
@@ -1,17 +1,20 @@
 const openBtn = document.querySelector(".menu-open");
 const closeBtn = document.querySelector(".menu-close");
 const mobMenu = document.querySelector(".mob-menu");
+const body = document.querySelector("body");
 
 const handleToggle = () => {
     console.log('menu', mobMenu)
   if (openBtn?.classList.contains("is-hidden-button")) {
     openBtn?.classList.remove("is-hidden-button");
+    body?.classList.remove("no-scroll");
       closeBtn?.classList.add("is-hidden-button");
       mobMenu?.classList.add("is-hidden");
   } else {
-      openBtn?.classList.add("is-hidden-button");
+    openBtn?.classList.add("is-hidden-button");
+    body?.classList.add("no-scroll");
       closeBtn?.classList.remove("is-hidden-button");
-      mobMenu?.classList.remove("is-hidden");
+    mobMenu?.classList.remove("is-hidden");
   }
 };
 
